e430e50bb1058e7d42499d04c5cb8e59945bfdc7
Pratik deokar <pratik.deokar@infosys.com>
2021-05-20 09:54:54 +0530

    first commit

---
+++ src/main/java/com/oms/product/ProductMsApplication.java
@@ -0,0 +1,15 @@
+package com.oms.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(value = {"classpath:messages.properties"})
+public class ProductMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMsApplication.class, args);
+	}
+
+}
